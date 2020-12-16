7f4ef42aa93734cd9dee3ce78412bfd849b7cd35
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-16 16:53:20 -0500

    did bubblesort

---
+++ Sorts.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Sorts{
+  /**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data){
+    for (int i = 0; i < data.length; i++) {
+      for (int j = 0; j < data.length - 1 - i; j++) {
+        if (data[j] > data[j + 1]) {
+          int temp = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
