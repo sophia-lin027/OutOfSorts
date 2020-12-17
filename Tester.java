5d178436277589d32129f7227a6ba072df831cb0
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-16 22:03:04 -0500

    more tests

--- Tester.java
+++ Tester.java
@@ -2,11 +2,15 @@ import java.util.Random;
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[] arr = new int[5];
+    int[] arr = new int[100];
     int a = (int)(Math.random()*1000);
     Random rng = new Random(a);
-    for(int i = 0; i < 5; i++ ){
+    for(int i = 0; i < 100; i++){
       arr[i] = (rng.nextInt() % 1000);
     }
+    Sorts n = new Sorts();
+    System.out.println("Before bubble sorting: " + Arrays.toString(arr));
+    n.bubbleSort(arr);
+    System.out.println("After bubble sorting: " +  Arrays.toString(arr));
   }
 }
