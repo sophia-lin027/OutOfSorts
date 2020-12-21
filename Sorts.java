c01d07c73b2efbc329da9ffb9b1dd35f51d91d56
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-12-21 13:21:02 -0500

    added insertionsort

--- Sorts.java
+++ Sorts.java
@@ -5,7 +5,7 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){
+  public static void bubbleSort(int[] data) {
     for (int i = 0; i < data.length; i++) {
       for (int j = 0; j < data.length - 1 - i; j++) {
         if (data[j] > data[j + 1]) {
@@ -14,6 +14,20 @@ public class Sorts{
           data[j + 1] = temp;
         }
       }
+    }
+  }
+
+  public static void insertionSort(int[] data) {
+    int index = 0;
+    for (int i = 0; i < data.length; i++)  {
+      int temp = data[i];
+      int j = i - 1;
+
+      while (j >= 0 && data[j] > temp) {
+        data[j + 1] = data[j];
+        j = j - 1;
+      }
+      data[j + 1] = temp;
     }
   }
 }
